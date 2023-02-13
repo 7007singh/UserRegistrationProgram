d97979e7d66cc40f1b83e871d51f5f3e7a1ec32c
7007singh <shw12021982@gmail.com>
2023-02-13 23:49:00 +0530

    Welcome to user registration program

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___Welcome to User Registration program---");
+        }
+    }
+}
